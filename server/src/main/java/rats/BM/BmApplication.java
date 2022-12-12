855e6bd967ab3a59a6041559ab46015170765f45
SeungJae <owen1743@naver.com>
2022-12-12 15:18:34 +0900

    start Spring

---
+++ server/src/main/java/rats/BM/BmApplication.java
@@ -0,0 +1,13 @@
+package rats.BM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmApplication.class, args);
+	}
+
+}
