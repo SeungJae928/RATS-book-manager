7e76d051920c366d4383709a194c37f5ce7bff42
SeungJae <owen1743@naver.com>
2022-12-12 21:14:32 +0900

    React - Spring 연결 완료

---
+++ server/src/main/java/rats/BM/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package rats.BM.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
