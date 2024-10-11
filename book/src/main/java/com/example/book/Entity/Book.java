41edeb7ad3db9f37526ff55cc12df42b19a63579
SeungJae <owen1743@naver.com>
2024-10-11 14:10:22 +0900

    Book Entity 생성

--- book/src/main/java/com/example/book/Entity/Book.java
+++ book/src/main/java/com/example/book/Entity/Book.java
@@ -1,5 +1,6 @@
 package com.example.book.Entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,10 +8,22 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "author")
     private String author;
+
+    @Column(name = "publisher")
     private String publisher;
+
+    @Column(name = "isbn")
     private String isbn;
 }
