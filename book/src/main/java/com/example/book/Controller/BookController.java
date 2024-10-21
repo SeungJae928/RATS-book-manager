5c82bcad372a6833b348511844b2a84652f258b9
SeungJae <owen1743@naver.com>
2024-10-21 15:32:18 +0900

    책 등록 Controller 구현

--- book/src/main/java/com/example/book/Controller/BookController.java
+++ book/src/main/java/com/example/book/Controller/BookController.java
@@ -1,7 +1,36 @@
 package com.example.book.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.book.DTO.BookRegistrationDTO;
+import com.example.book.Service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/Books")
 public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerBook(@RequestBody BookRegistrationDTO bookRegistrationDTO) {
+        String resISBN = null;
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        try {
+            resISBN = bookService.registerBook(bookRegistrationDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .headers(header)
+                    .body(e.getMessage());
+        }
+        return ResponseEntity.ok()
+                .headers(header)
+                .body(resISBN);
+    }
 }
